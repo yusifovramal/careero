c562668117c2218fad877b6281f74d1ea7600a6c
Ramal <ramal.github@gmail.com>
2025-12-30 21:30:57 +0400

    feat: design login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,24 @@
 
-function Login() {
-  return (
-    <div>Login</div>
-  )
-}
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import Logo from "@/public/logo.png";
+import LoginForm from "@/components/forms/login-form";
 
-export default Login
+const LoginPage = () => {
+  return (
+    <div className="min-h-screen w-screen flex items-center justify-center">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <Link href="/" className="flex items-center gap-2 self-center">
+          <Image src={Logo} alt="Logo" className="size-10" />
+          <h1 className="text-2xl font-bold">
+            Job<span className="text-primary">Marshal</span>
+          </h1>
+        </Link>
+        <LoginForm />
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
